1119e519ee76fe041118481573c3a9d133f2582d
Freivin Campbell <freivin12@gmail.com>
2022-07-18 21:56:13 -0600

    fixing mail to email

--- be/app/mailers/contact_form_mailer.rb
+++ be/app/mailers/contact_form_mailer.rb
@@ -3,8 +3,8 @@ class ContactFormMailer < ApplicationMailer
     puts params[:contact_form]
     @contact_form = params[:contact_form]
 
-    mail(to: 'freivin12@gmail.com', subject: "You have a new contact form message")
-    # mail(to: 'info@genderrevealgame.com', subject: "You have a new contact form message")
+    # mail(to: 'freivin12@gmail.com', subject: "You have a new contact form message")
+    mail(to: 'info@genderrevealgame.com', subject: "You have a new contact form message")
 
     @contact_form.status = "sent"
     @contact_form.save
